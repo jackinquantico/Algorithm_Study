0893d0feb45a909ea5b42477eb22073fd9b32468
jackinquantico <jackinquantico@naver.com>
2023-01-04 13:40:01 +0900

    프로그래머스 0단계 풀기

---
+++ 01_DataStructure/src/com/jk/programmers/P120802.java
@@ -0,0 +1,13 @@
+package com.jk.programmers;
+
+public class P120802 {
+	
+    public int solution(int num1, int num2) {
+        int answer = -1;
+        
+        answer = num1 + num2;
+        
+        return answer;
+    }
+
+}
---
+++ 01_DataStructure/src/com/jk/programmers/P120803.java
@@ -0,0 +1,12 @@
+package com.jk.programmers;
+
+public class P120803 {
+
+    public int solution(int num1, int num2) {
+        int answer = 0;
+        
+        answer = num1 - num2;
+        
+        return answer;
+    }
+}
