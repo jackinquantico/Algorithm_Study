36c806cd19ae4a9653a051b082ad46d650a31086
jackinquantico <jackinquantico@naver.com>
2023-01-04 13:40:09 +0900

    프로그래머스 0단계 풀기

---
+++ 01_DataStructure/src/com/jk/programmers/P120806.java
@@ -0,0 +1,12 @@
+package com.jk.programmers;
+
+public class P120806 {
+
+    public int solution(int num1, int num2) {
+        int answer = 0;
+        
+        answer = (int)(((float)num1 / num2) * 1000);
+        
+        return answer;
+    }
+}
