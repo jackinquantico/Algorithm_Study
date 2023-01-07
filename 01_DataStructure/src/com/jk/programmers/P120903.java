8c0f8240c742dff56d271c5be808be22d8e3900f
jackinquantico <jackinquantico@naver.com>
2023-01-07 15:05:06 +0900

    import 구문 추가

--- 01_DataStructure/src/com/jk/programmers/P120903.java
+++ 01_DataStructure/src/com/jk/programmers/P120903.java
@@ -1,5 +1,20 @@
 package com.jk.programmers;
 
-public class P120903 {
+import java.util.HashSet;
 
+public class P120903 {
+    public int solution(String[] s1, String[] s2) {
+        
+        HashSet<String> set = new HashSet<>();
+        
+        for (int i=0; i<s1.length; i++) {
+        	set.add(s1[i]);
+        }
+        
+        for (int i=0; i<s2.length; i++) {
+        	set.add(s2[i]);
+        }
+        
+        return s1.length + s2.length - set.size();
+    }
 }
